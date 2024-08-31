85019216d1e62d7c15e88f74c01170b646e011fd
kristofferwaagen <kristofferwagen@gmail.com>
2024-08-31 13:10:14 +0200

    Added basic "Hello World" greeting

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/")
+    public String greeting() {
+        return "Hello World";
+    }
+}
